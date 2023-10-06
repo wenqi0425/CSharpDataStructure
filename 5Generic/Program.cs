1552f7be0fd66a68f537a109876ce70fc202d10c
wenqi0425 <xiaoc288@gmail.com>
2023-10-06 16:51:00 +0200

    Generic test

--- 5Generic/Program.cs
+++ 5Generic/Program.cs
@@ -4,25 +4,23 @@
 
 int[] n = { 1,2,3,4,5,6,7 };
 
-GenericArray<int> array = new GenericArray<int>();
+GenericArray<int> arr1 = new GenericArray<int>();
 
 for (int i = 0; i < n.Length; i++)
-    array.AddLast(n[i]);
+    arr1.AddLast(n[i]);
 
-Console.WriteLine(array);
-
-
+Console.WriteLine(arr1);  // Array: count=7 capacity=10，[1, 2, 3, 4, 5, 6, 7]
 
 
+// 测试 string
 
+string[] s = { "a", "b", "c", "d", "e", "f", "g" };
 
+GenericArray<string> arr2 = new GenericArray<string>();
 
+for (int i = 0; i < s.Length; i++)
+    arr2.AddLast(s[i]);
 
-
-
-
-
-
-
+Console.WriteLine(arr2);  // Array: count=7 capacity=10, [a, b, c, d, e, f, g]
 
 Console.Read();
